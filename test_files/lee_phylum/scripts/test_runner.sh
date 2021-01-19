f0a4b15b5e06def34aa3e39f7a9593d230bfe0ae
Ryan Moore <moorer@udel.edu>
2021-01-19 16:56:56 -0500

    Auto check lee phylum test correctness

--- test_files/lee_phylum/scripts/test_runner.sh
+++ test_files/lee_phylum/scripts/test_runner.sh
@@ -1,8 +1,11 @@
 LEE_PHYLUM_DIR="./test_files/lee_phylum"
+PREVIOUS_OUTPUT="${LEE_PHYLUM_DIR}/expected_output"
 
 # Try to delete output files.
 rm "${LEE_PHYLUM_DIR}/lee_phylum_divnet_output.csv" \
   "${LEE_PHYLUM_DIR}/lee_phylum_divnet_plot.png"
 
 "${DIVNET_RS}" "${LEE_PHYLUM_DIR}/config_lee_phylum.toml" && \
-  "${RSCRIPT}" --vanilla "${LEE_PHYLUM_DIR}/scripts/make_plot.R" "${LEE_PHYLUM_DIR}"
+  "${RSCRIPT}" --vanilla "${LEE_PHYLUM_DIR}/scripts/make_plot.R" "${LEE_PHYLUM_DIR}" && \
+  diff "${PREVIOUS_OUTPUT}/lee_phylum_divnet_output.csv" "${LEE_PHYLUM_DIR}/lee_phylum_divnet_output.csv" && \
+  diff "${PREVIOUS_OUTPUT}/lee_phylum_divnet_plot.png" "${LEE_PHYLUM_DIR}/lee_phylum_divnet_plot.png"
